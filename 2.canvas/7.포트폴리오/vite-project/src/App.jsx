fcbad772ca63f077790936e25bf8ab052320675a
chxxwn <lisa9902@naver.com>
2024-10-16 19:23:32 +0900

    포트폴리오

--- 2.canvas/7.포트폴리오/vite-project/src/App.jsx
+++ 2.canvas/7.포트폴리오/vite-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import ArrowImg from './assets/arrow.svg'
 import Nudake from './components/Nudake'
+import RotateCanvas from './components/RotateCanvas'
 
 function App() {
   return (
@@ -36,15 +37,7 @@ function App() {
           </article>
         </section>
         <section className="section-4">
-          <canvas></canvas>
-          <aside>
-            <h1>JavaScript</h1>
-            <h2>⭐️⭐️⭐️⭐️⭐️</h2>
-            <p>
-              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as
-              their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-            </p>
-          </aside>
+          <RotateCanvas />
         </section>
       </div>
       <footer>
